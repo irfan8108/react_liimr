021fd0cd75b65cbe8864ec4443d658850b8ebde3
irfan <irfan8108@gmail.com>
2018-01-27 14:48:36 +0530

    v_1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+setInterval(function(){
+  ReactDOM.render(
+    clock(),
+    document.getElementById('root')
+  );
+},1000);
+
 let clock = function(){
   return <h1>{new Date().toLocaleTimeString()}</h1>
 }
-
-ReactDOM.render(
-  clock(),
-  document.getElementById('root')
-);
