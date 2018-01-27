1e84836ae47af29d8124039230d018c920904847
irfan <irfan8108@gmail.com>
2018-01-27 15:14:18 +0530

    class function with same name

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-setInterval(function(){
-  ReactDOM.render(
-    clock(),
-    document.getElementById('root')
-  );
-},1000);
-
-let clock = function(){
-  return <h1>{new Date().toLocaleTimeString()}</h1>
+class Liimr extends React.Component {
+  render(){
+    return <h1>LA IlahaIlLalLah...</h1>
+  }
 }
+
+function Liimr(props){
+  return <h1>
+  Darood Shareef:&nbsp;
+  {props.title}
+  </h1>
+}
+
+ReactDOM.render(
+  <Liimr title = 'DeepDiveinTo'/>,
+  document.getElementById('root')
+);
